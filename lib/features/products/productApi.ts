c632f4b1a7f70c1bfb91b393d20b573fa6e98db3
bomnorng.soeurt <soeurtbomnorng3@gmail.com>
2026-03-13 19:39:23 +0700

    commit query

---
+++ lib/features/products/productApi.ts
@@ -0,0 +1,30 @@
+import { ProductResponse } from "@/lib/types/product-type";
+import { fakeStoreApi } from "../api/api";
+
+export const productApi = fakeStoreApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query<ProductResponse[], void>({
+      query: () => "/products",
+      providesTags: ["products"],
+    }),
+    getProductById: builder.query<ProductResponse, number>({
+      query: (id) => `/products/${id}`,
+      providesTags: ["products"],
+    }),
+    addProduct: builder.mutation({
+      query: (newProduct) => ({
+        url: "/products",
+        method: "POST",
+        body: newProduct,
+      }),
+      invalidatesTags: ["products"],
+    }),
+  }),
+});
+
+// export hooks for ui component can use it
+export const {
+  useGetProductsQuery,
+  useGetProductByIdQuery,
+  useAddProductMutation,
+} = productApi;
--- lib/store.ts
+++ lib/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterSlice } from "./features/counter/counterSlice";
+import { productApi } from "./features/products/productApi";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       counter: counterSlice.reducer,
+      [productApi.reducerPath]: productApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(productApi.middleware),
   });
 };
 
